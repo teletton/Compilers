0e8efc700a933fff0e8731fee5a721b93d383030
iletavcioski <it8816@student.uni-lj.si>
2021-04-22 16:24:16 +0200

    Small fixes

--- prev21/src/prev/phase/imclin/StmtCan.java
+++ prev21/src/prev/phase/imclin/StmtCan.java
@@ -37,17 +37,18 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
         Vector<ImcStmt> prvStmt = svec.pop();
         binOp.sndExpr.accept(this, visArg);
         MemTemp vtor = new MemTemp();
+        ImcTEMP pr = new ImcTEMP(prv);
+        ImcTEMP vt = new ImcTEMP(vtor);
         ImcExpr vtorexpr = stexpr.pop();
         Vector<ImcStmt> vtorStmt = svec.pop();
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
         stmt.addAll(prvStmt);
-        stmt.add(new ImcMOVE(new ImcTEMP(prv), prvexpr));
+        stmt.add(new ImcMOVE(pr, prvexpr));
         stmt.addAll(vtorStmt);
-        stmt.add(new ImcMOVE(new ImcTEMP(vtor), vtorexpr));
-
-        stexpr.push(new ImcBINOP(binOp.oper, new ImcTEMP(prv), new ImcTEMP(vtor)));
+        stmt.add(new ImcMOVE(vt, vtorexpr));
+        stexpr.push(new ImcBINOP(binOp.oper, pr, vt));
         svec.push(stmt);
-        return null;
+        return stmt;
     }
 
     @Override
@@ -55,34 +56,30 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
         //System.out.println("IMCCALL");
         Vector<ImcExpr> args = new Vector<ImcExpr>();
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
-
         for (int i = 0; i < call.args.size(); i++) {
             Vector<ImcStmt> aStmt = new Vector<ImcStmt>();
-
             if (i == 0) {
                 MemTemp temp = new MemTemp();
-                aStmt.add(new ImcMOVE(new ImcTEMP(temp), call.args.get(i)));
-                args.add(new ImcTEMP(temp));
+                ImcTEMP tt = new ImcTEMP(temp);
+                aStmt.add(new ImcMOVE(tt, call.args.get(i)));
+                args.add(tt);
                 stmt.addAll(aStmt);
                 continue;
             }
-
             ImcExpr expr = call.args.get(i);
             expr.accept(this, visArg);
             MemTemp argv = new MemTemp();
+            ImcTEMP argvt = new ImcTEMP(argv);
             ImcExpr argExpr = stexpr.pop();
             Vector<ImcStmt> argStmt = svec.pop();
-
             aStmt.addAll(argStmt);
-            aStmt.add(new ImcMOVE(new ImcTEMP(argv), argExpr));
-
-            args.add(new ImcTEMP(argv));
+            aStmt.add(new ImcMOVE(argvt, argExpr));
+            args.add(argvt);
             stmt.addAll(aStmt);
         }
-
         stexpr.push(new ImcCALL(call.label, call.offs, args));
         svec.push(stmt);
-        return null;
+        return stmt;
     }
 
     @Override
@@ -90,28 +87,27 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
         //System.out.println("IMCCJUMP");
         cjump.cond.accept(this, visArg);
         MemTemp temp = new MemTemp();
+        ImcTEMP tt = new ImcTEMP(temp);
         ImcExpr cExpr = stexpr.pop();
         Vector<ImcStmt> cStmt = svec.pop();
-
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
         stmt.addAll(cStmt);
-        stmt.add(new ImcMOVE(new ImcTEMP(temp), cExpr));
-
+        stmt.add(new ImcMOVE(tt, cExpr));
         MemLabel fLabel = new MemLabel();
-        stmt.add(new ImcCJUMP(new ImcTEMP(temp), cjump.posLabel, fLabel));
+        stmt.add(new ImcCJUMP(tt, cjump.posLabel, fLabel));
         stmt.add(new ImcLABEL(fLabel));
         stmt.add(new ImcJUMP(cjump.negLabel));
-
         svec.push(stmt);
-        return null;
+        return stmt;
     }
 
     @Override
     public Vector<ImcStmt> visit(ImcCONST constant, Object visArg) {
         //System.out.println("IMCCONST");
-        stexpr.push(new ImcCONST(constant.value));
-        svec.push(new Vector<ImcStmt>());
-        return null;
+        Vector<ImcStmt> vist = new Vector<ImcStmt>();
+        stexpr.push(constant);
+        svec.push(vist);
+        return vist;
     }
 
     @Override
@@ -123,12 +119,9 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
 
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
         stmt.addAll(exStmt);
-
-        /// execute the expression in case it modifies the environment.
         stmt.add(new ImcMOVE(new ImcTEMP(new MemTemp()), exExpr));
-
         svec.push(stmt);
-        return null;
+        return stmt;
     }
 
     @Override
@@ -136,18 +129,16 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
         //System.out.println("IMCJUMP");
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
         stmt.add(new ImcJUMP(jump.label));
-
         svec.push(stmt);
-        return null;
+        return stmt;
     }
 
     public Vector<ImcStmt> visit(ImcLABEL label, Object visArg) {
         //System.out.println("IMCLABEL");
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
         stmt.add(new ImcLABEL(label.label));
-
         svec.push(stmt);
-        return null;
+        return stmt;
     }
 
     @Override
@@ -155,16 +146,15 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
         //System.out.println("IMCMEM");
         mem.addr.accept(this, visArg);
         MemTemp temp = new MemTemp();
+        ImcTEMP tt = new ImcTEMP(temp);
         ImcExpr addrExpr = stexpr.pop();
         Vector<ImcStmt> addrStmt = svec.pop();
-
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
         stmt.addAll(addrStmt);
-        stmt.add(new ImcMOVE(new ImcTEMP(temp), addrExpr));
-
-        stexpr.push(new ImcMEM(new ImcTEMP(temp)));
+        stmt.add(new ImcMOVE(tt, addrExpr));
+        stexpr.push(new ImcMEM(tt));
         svec.push(stmt);
-        return null;
+        return stmt;
     }
 
     @Override
@@ -185,14 +175,10 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
             stmt.addAll(dstStmt);
             stmt.addAll(srcStmt);
             ImcTEMP sr = new ImcTEMP(src);
-            
             stmt.add(new ImcMOVE(sr, srcExpr));
-            
             stmt.add(new ImcMOVE(dstExpr, sr));
-
             svec.push(stmt);
-
-            return null;
+            return stmt;
         }
 
         move.dst.accept(this, visArg);
@@ -204,7 +190,6 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
         MemTemp src = new MemTemp();
         ImcExpr srcExpr = stexpr.pop();
         Vector<ImcStmt> srcStmt = svec.pop();
-
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
         stmt.addAll(dstStmt);
         stmt.add(new ImcMOVE(new ImcTEMP(dst), dstExpr));
@@ -214,17 +199,17 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
         ImcTEMP ds = new ImcTEMP(dst);
         System.out.println(ds);
         stmt.add(new ImcMOVE(ds, sr));
-
         svec.push(stmt);
-        return null;
+        return stmt;
     }
 
     @Override
     public Vector<ImcStmt> visit(ImcNAME name, Object visArg) {
         //System.out.println("IMCNAME");
-        stexpr.push(new ImcNAME(name.label));
-        svec.push(new Vector<ImcStmt>());
-        return null;
+        Vector<ImcStmt> vist = new Vector<ImcStmt>();
+        stexpr.push(name);
+        svec.push(vist);
+        return vist;
     }
 
     @Override
@@ -232,30 +217,28 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
         //System.out.println("IMCSEXPR");
         sExpr.stmt.accept(this, visArg);
         Vector<ImcStmt> sStmt = svec.pop();
-
         sExpr.expr.accept(this, visArg);
         MemTemp temp = new MemTemp();
+        ImcTEMP tt = new ImcTEMP(temp);
         ImcExpr sexprExpr = stexpr.pop();
         Vector<ImcStmt> sexprStmt = svec.pop();
-
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
         stmt.addAll(sStmt);
         stmt.addAll(sexprStmt);
-        stmt.add(new ImcMOVE(new ImcTEMP(temp), sexprExpr));
-
-        stexpr.push(new ImcTEMP(temp));
+        stmt.add(new ImcMOVE(tt, sexprExpr));
+        stexpr.push(tt);
         svec.push(stmt);
-        return null;
+        return stmt;
     }
 
     @Override
     public Vector<ImcStmt> visit(ImcTEMP temp, Object visArg) {
         //System.out.println("IMCTEMP");
         //System.out.println(((MemTemp)temp.temp).temp);
-
-        stexpr.push(new ImcTEMP(temp.temp));
-        svec.push(new Vector<ImcStmt>());
-        return null;
+        Vector<ImcStmt> vist = new Vector<ImcStmt>();
+        stexpr.push(temp);
+        svec.push(vist);
+        return vist;
     }
 
     @Override
@@ -263,16 +246,17 @@ public class StmtCan implements ImcVisitor<Vector<ImcStmt>, Object> {
         //System.out.println("IMCUNOP");
         unOp.subExpr.accept(this, visArg);
         MemTemp temp = new MemTemp();
+        ImcTEMP tt = new ImcTEMP(temp);
         ImcExpr subExpr = stexpr.pop();
         Vector<ImcStmt> subStmt = svec.pop();
 
         Vector<ImcStmt> stmt = new Vector<ImcStmt>();
         stmt.addAll(subStmt);
-        stmt.add(new ImcMOVE(new ImcTEMP(temp), subExpr));
+        stmt.add(new ImcMOVE(tt, subExpr));
 
-        stexpr.push(new ImcUNOP(unOp.oper, new ImcTEMP(temp)));
+        stexpr.push(new ImcUNOP(unOp.oper, tt));
         svec.push(stmt);
 
-        return null;
+        return stmt;
     }
 }
